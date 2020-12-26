76ae29143f91d5691f4edc2ed9dc7e80112b87aa
Roee Weiss Lipshitz <39015778+Roeezz@users.noreply.github.com>
2020-12-26 23:33:20 +0200

    Add data saved for heatmaps in Q4d, and answers in the anser doc

--- ass2/Q3/check_grid.m
+++ ass2/Q3/check_grid.m
@@ -3,9 +3,7 @@ function label_table = check_grid(alpha, sigma, Xtrain, Xgrid, Ygrid)
     m = size(Xtrain,1);
     label_table = zeros(h,w);
     for i=1:h
-        disp(fprintf("i = %u ", i));
         for j=1:w
-            disp(fprintf("j = %u ", j));
             for k=1:m
                 label_table(i,j) = label_table(i,j) + alpha(k)*rbfkernel(sigma,Xtrain(k,:), [Xgrid(i,j) Ygrid(i,j)]);
             end
--- ass2/Q3/draw_heatmap.m
+++ ass2/Q3/draw_heatmap.m
@@ -1,15 +1,13 @@
 function h=draw_heatmap(xvals, yvals, label_table, Xtrain, Ytrain)
-    Xpos = Xtrain(Ytrain > 0,:);
-    Xneg = Xtrain(Ytrain < 0,:);
-    
-    h = heatmap(xvals, yvals, label_table);
+    h = heatmap(label_table);
     colormapeditor
     grid off
-    colormapeditor
     hs = struct(h);
-    warning(old_warning_state);
     hs.XAxis.TickValues = [];
     hs.YAxis.TickValues = [];
+    
+    Xpos = Xtrain(Ytrain > 0,:);
+    Xneg = Xtrain(Ytrain < 0,:);
     ax = axes();
     hold on
     plot(Xpos(:,1), Xpos(:,2), 'whiteo', 'MarkerFaceColor', 'w');
